8853622b66853a3c7b5cceea06d81e74fdf89938
John Maruska <jackmaruska@gmail.com>
2020-10-25 16:19:00 -0500

    start phasing out discord.clj

--- src/clj/hub/discord/core.clj
+++
@@ -1,8 +0,0 @@
-(ns hub.discord.core
-  "Commands for running the discord bot."
-  (:require [discord.bot :as bot]))
-
-;; extensions are added to a registry with defextension, they don't have to be
-;; passed to start in any way.
-
-(defn start! [& args] (bot/start))
