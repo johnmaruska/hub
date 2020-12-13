2e84ed20ffb7358073f4fb699b1d97a81c576fee
johnmaruska <jackmaruska@gmail.com>
2020-12-13 16:07:37 -0600

    Add causal-history example + tests

---
+++ src/clj/hub/logical_clocks/causal_history.clj
@@ -0,0 +1,37 @@
+(ns hub.logical-clocks.causal-history
+  "Basic implementation of Figure 2 causal histories approach in ACM's
+  Why Logical Clocks are Easy  https://queue.acm.org/detail.cfm?id=2917756"
+  (:require [clojure.set :as set])
+  (:refer-clojure :exclude [send]))
+
+(defn init-node [state nid]
+  (-> state
+      (assoc-in [:history nid 1] #{[nid 1]})
+      (assoc-in [:node nid] 1)))
+
+(defn version [state nid]
+  (get-in state [:node nid]))
+
+(defn history [state nid]
+  (get-in state [:history nid (version state nid)]))
+
+(defn add-event [state [nid ver] history]
+  (-> state
+      (assoc-in [:history nid ver]
+                (set/union history #{[nid ver]}))
+      (assoc-in [:node nid] ver)))
+
+;;;; interface
+
+(defn send [state src-nid dest-nid]
+  (let [event [dest-nid (inc (version state dest-nid))]]
+    (add-event state event
+               (set/union (history state dest-nid)
+                          (history state src-nid)))))
+
+(defn bump [state nid]
+  (let [event [nid (inc (version state nid))]]
+    (add-event state event (history state nid))))
+
+(defn caused-by? [state end-event start-event]
+  (contains? (get-in (:history state) end-event) start-event))
---
+++ test/clj/hub/logical_clocks/causal_history_test.clj
@@ -0,0 +1,52 @@
+(ns hub.logical-clocks.causal-history-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [hub.logical-clocks.causal-history :as sut]
+   [hub.logical-clocks.util :as util]))
+
+(declare example)
+
+(deftest add-event-test
+  (testing "happy path"
+    (is (= {:node {:a 2}
+            :history {:a {1 #{[:a 1]}
+                          2 #{[:a 1] [:a 2]}}}}
+           (sut/add-event {:node {:a 1}
+                           :history {:a {1 #{[:a 1]}}}}
+                          [:a 2]
+                          #{[:a 1]})))))
+
+(deftest bump-test
+  (testing "happy path"
+    (let [state-a1 (sut/init-node {} :a)
+          state-a2 {:node {:a 2}
+                    :history {:a {1 #{[:a 1]}
+                                  2 #{[:a 1] [:a 2]}}}}]
+      (is (= state-a2 (sut/bump state-a1 :a))))))
+
+(deftest full-interface
+  (let [simulated-example (util/simulate {:send sut/send
+                                          :bump sut/bump
+                                          :init-node sut/init-node})]
+    (testing "Full simulation of Figure 2 matches example structure above"
+      (is (= example simulated-example)))
+    (testing "Given caused-by? example works"
+      (is (sut/caused-by? simulated-example [:c 3] [:a 1])))))
+
+(def example
+  {:node {:a 3 :b 3 :c 3}
+
+   :history
+   {:a {1 #{[:a 1]}
+        2 #{[:a 1] [:a 2]}
+        3 #{[:a 1] [:a 2] [:a 3]}}
+    :b {1 #{[:b 1]}
+        2 #{[:a 1] [:a 2]
+            [:b 1] [:b 2]}
+        3 #{[:a 1] [:a 2]
+            [:b 1] [:b 2] [:b 3]}}
+    :c {1 #{[:c 1]}
+        2 #{[:c 1] [:c 2]}
+        3 #{[:a 1] [:a 2]
+            [:b 1] [:b 2] [:b 3]
+            [:c 1] [:c 2] [:c 3]}}}})
