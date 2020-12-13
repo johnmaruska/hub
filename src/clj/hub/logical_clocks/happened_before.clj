216b784a355f4e77870ac64fe5daab3e85f1729e
johnmaruska <jackmaruska@gmail.com>
2020-12-13 16:07:11 -0600

    Add tested to happened-before

--- src/clj/hub/logical_clocks/happened_before.clj
+++ src/clj/hub/logical_clocks/happened_before.clj
@@ -3,73 +3,45 @@
   Why Logical Clocks are Easy  https://queue.acm.org/detail.cfm?id=2917756
 
   This is useful pretty much exclusively for full-knowledge analysis, and not
-  very efficient for that. Best used for illustration not computation.")
+  very efficient for that. Best used for illustration not computation."
+  (:refer-clojure :exclude [send]))
 
-(defn init-node! [state nid]
+(defn init-node [state nid]
   (-> state
       (assoc-in [:goes-to nid 1] [])
       (assoc-in [:node nid] {:nid nid :version 1})))
 
-(defn init-event! [state [nid version]]
-  (-> state
-      (assoc-in [:goes-to nid version] [])
-      (assoc-in [:node nid :version] version)))
-
-(defn happened-before! [state src-event dest-event]
-  (let [[src-nid src-ver] src-event]
+(defn happened-before [state src-event dest-event]
+  (let [[src-nid src-ver] src-event
+        [dest-nid dest-ver] dest-event]
     (-> state
-        (init-event! dest-event)
+        ;; bump destination node to destination event
+        (assoc-in [:node dest-nid :version] dest-ver)
+        ;; destination event does not yet go anywhere
+        (assoc-in [:goes-to dest-nid dest-ver] [])
+        ;; source event goes to destination event
         (update-in [:goes-to src-nid src-ver]
                    conj dest-event))))
 
-(defn send! [state src-nid dest-nid]
+;;;; interface
+
+(defn send [state src-nid dest-nid]
   (let [dest-version (:version (get-in state [:node dest-nid]))
         src-event    [src-nid  (:version (get-in state [:node src-nid]))]
         dest-event   [dest-nid dest-version]
         result-event [dest-nid (inc dest-version)]]
     (-> state
-        (happened-before! src-event  result-event)
-        (happened-before! dest-event result-event))))
+        (happened-before src-event  result-event)
+        (happened-before dest-event result-event))))
 
-(defn bump! [state nid]
+(defn bump [state nid]
   (let [version    (:version (get-in state [:node nid]))
         src-event  [nid version]
         dest-event [nid (inc version)]]
-    (happened-before! state src-event dest-event)))
+    (happened-before state src-event dest-event)))
 
 (defn goes-to? [state start-event end-event]
   (if (= end-event start-event)
     true
     (some #(goes-to? state % end-event)
           (get-in (:goes-to state) start-event))))
-
-(defn caused-by? [state end-event start-event]
-  (goes-to? state start-event end-event))
-
-
-(def example-state
-  {:node    {:a {:nid     :a
-                 :version 3}
-             :b {:nid     :b
-                 :version 3}
-             :c {:nid     :c
-                 :version 3}}
-   :goes-to {:a {1 [[:a 2]]
-                 2 [[:a 3] [:b 2]]
-                 3 []}
-             :b {1 [[:b 2]]
-                 2 [[:b 3]]
-                 3 [[:c 3]]}
-             :c {1 [[:c 2]]
-                 2 [[:c 3]]
-                 3 []}}})
-
-(defn verify []
-  (-> (reduce init-node! {} [:a :b :c])
-      (bump! :a)
-      (send! :a :b)
-      (bump! :c)
-      (bump! :a)
-      (bump! :b)
-      (send! :b :c)
-      (goes-to? [:a 1] [:c 3])))
---
+++ test/clj/hub/logical_clocks/happened_before_test.clj
@@ -0,0 +1,33 @@
+(ns hub.logical-clocks.happened-before-test
+  (:require
+   [clojure.test :refer [deftest is testing]]
+   [hub.logical-clocks.happened-before :as sut]
+   [hub.logical-clocks.util :as util]))
+
+(declare example)
+
+(deftest full-interface
+  (let [simulated-example (util/simulate {:send sut/send
+                                          :bump sut/bump
+                                          :init-node sut/init-node})]
+    (testing "Full simulation of Figure 2 matches example structure above"
+      (is (= example simulated-example)))
+    (testing "Given caused-by? example works"
+      (is (sut/goes-to? simulated-example [:a 1] [:c 3])))))
+
+(def example
+  {:node
+   {:a {:nid :a :version 3}
+    :b {:nid :b :version 3}
+    :c {:nid :c :version 3}}
+
+   :goes-to
+   {:a {1 [[:a 2]]
+        2 [[:b 2] [:a 3]]
+        3 []}
+    :b {1 [[:b 2]]
+        2 [[:b 3]]
+        3 [[:c 3]]}
+    :c {1 [[:c 2]]
+        2 [[:c 3]]
+        3 []}}})
