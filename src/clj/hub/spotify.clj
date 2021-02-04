cc33759bbe2dc3f6b550f9a7362716d7a63fff5d
John Maruska <jackmaruska@gmail.com>
2021-02-03 19:14:40 -0600

    Spotify root filaRemove unused root spotify.clj

--- src/clj/hub/spotify.clj
+++
@@ -1,9 +0,0 @@
-(ns hub.spotify
-  (:require
-   [hub.spotify.playlist :as playlist]
-   [hub.spotify.tracks :as tracks]
-   [hub.spotify.util :refer [crawl! get! api]]
-   [hub.util :refer [find-by]]))
-
-(defn new-releases []
-  (crawl! (api "/v1/browse/new-releases")))
