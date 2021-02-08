20a2dd335477ddcab9c8dc5ce0d9d351678a1599
John Maruska <jackmaruska@gmail.com>
2021-02-07 18:21:40 -0600

    Refactor discljord commands (#10)
    * Mid-flight bowling kata
    
    * Change commands to work via prefix isntead of that command macro

---
+++ src/clj/hub/discljord/commands.clj
@@ -0,0 +1,22 @@
+(ns hub.discljord.commands
+  (:require
+   [clojure.string :as string]
+   [hub.discljord.admin :as admin]
+   [hub.discljord.guess-that-sound :as guess-that-sound]
+   [hub.discljord.minesweeper :as minesweeper]))
+
+(def prefix
+  {;; Admin
+   "!bot please be kill"    (fn [& _]
+                              (throw (ex-info "ManualKill" {:manual-kill? true})))
+   "!working"               #'admin/working
+   ;; Guess that sound
+   "!play guess-that-sound" #'guess-that-sound/start!
+   "!guess"                 #'guess-that-sound/guess!
+   "!answer"                #'guess-that-sound/answer!
+   ;; Misc
+   "!minesweep"             #'minesweeper/spit-board})
+
+(defn matching-prefixes [event]
+  (filter #(string/starts-with? (:content event) %)
+          (keys prefix)))
