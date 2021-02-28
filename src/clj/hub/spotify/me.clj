d91cc1c4bea0e73886e24c20e804af892c1ee43c
John Maruska <jackmaruska@gmail.com>
2021-02-27 22:02:43 -0600

    Memoize large retrievals from Spotify API

--- src/clj/hub/spotify/me.clj
+++ src/clj/hub/spotify/me.clj
@@ -2,27 +2,30 @@
   (:require
    [hub.spotify.util :refer [api crawl! get!]]))
 
-(defn all-my [entity]
-  (crawl! (api "/v1/me/" entity "?limit=50")))
+(def all-my
+  (memoize
+   (fn all-my* [entity]
+     (crawl! (api "/v1/me/" entity "?limit=50")))))
 
 (def playlists (partial all-my "playlists"))
 (def tracks    (partial all-my "tracks"))
 (def shows     (partial all-my "shows"))
 
-;; This times out the REPL because of how many saved songs I have...
 (defn artists []
-  (->> (all-my "tracks")  ; (:items (get! (api "/v1/me/tracks?limit=50")))
+  (->> (all-my "tracks")
        (map (comp :artists :track))
        (apply concat)
        distinct))
 
-(defn my-top
+(def my-top
   "entity may be `tracks` or `artists`
   time-range may be `long_term`, `medium_term`, `short_term`"
-  [entity & {:keys [time-range]}]
-  (let [query-str (when time-range
-                    (str "?time_range=" time-range))]
-    (all-my (str "top/" entity query-str))))
+  (memoize
+   (fn my-top*
+     [entity & {:keys [time-range]}]
+     (let [query-str (when time-range
+                       (str "?time_range=" time-range))]
+       (all-my (str "top/" entity query-str))))))
 
 (def top-tracks  (partial my-top "tracks"))
 (def top-artists (partial my-top "artists"))
--- src/clj/hub/spotify/tracks.clj
+++ src/clj/hub/spotify/tracks.clj
@@ -10,13 +10,22 @@
                  (assoc acc k (first v)))
                {})))
 
-(defn audio-features
+(defn audio-features*
   [tracks]
   {:pre [(>= 100 (count tracks))]}
   (let [ids (->> tracks
                  (map (comp :id :track))
                  (string/join ","))]
     (:audio_features (get! (api "/v1/audio-features?ids=" ids)))))
+
+(defn audio-features
+  "Request audio features for all required tracks.
+  Will chunk requests that are too large and recombine them."
+  [tracks]
+  (->> tracks
+       (partition 100)
+       (map audio-features*)  ; TODO: parallelize
+       (apply concat)))
 
 (defn enrich
   "Request audio features for a collection of tracks."
