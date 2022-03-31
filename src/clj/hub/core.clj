fcaecdb7eeabfcae0705c4d361c9aa834dabf36d
John Maruska <jackmaruska@gmail.com>
2022-03-31 14:28:19 -0500

    Move discord run to its own main

--- src/clj/hub/core.clj
+++ src/clj/hub/core.clj
@@ -6,7 +6,6 @@
    [hub.spotify :as spotify]
    [hub.discljord.core :as discord]
    [hub.server :as server]
-   [hub.util :refer [swallow-exception]]
    [mount.core :as mount :refer [defstate]]
    [clojure.tools.logging :as log])
   (:gen-class))
@@ -14,25 +13,6 @@
 (defstate webserver
   :start (server/start!)
   :stop  (.stop webserver))
-
-(defmacro spin-forever [& body]
-  `(loop [] ~@body (recur)))
-
-(def manual-kill? (comp :manual-kill? ex-data))
-
-(defmacro spin-until-manual-kill [& body]
-  `(swallow-exception manual-kill?
-     (spin-forever
-      (swallow-exception (comp not manual-kill?)
-        ~@body))))
-
-(defn discord-run! []
-  (let [discord-bot (discord/start!)]
-    (try
-      (spin-until-manual-kill
-       (discord/handle-event! discord-bot))
-      (finally
-        (discord/stop! discord-bot)))))
 
 ;; TODO: proper monorepo doesn't just switch on command
 (defn -main [command & args]
@@ -42,5 +22,5 @@
     "id3"     (apply id3/main args)
     "spotify" (apply spotify/main args)
     "server"  (mount/start)
-    "discord" (discord-run!)
+    "discord" (apply discord/main args)
     (log/error "command must be one of [conway dice id3 spotify server]")))
