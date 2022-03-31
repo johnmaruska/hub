53753329398091240f0d5d32ef4d7f39f5895a1c
John Maruska <jackmaruska@gmail.com>
2022-03-31 14:36:01 -0500

    Simplify main dispatch, use map instead of case

--- src/clj/hub/core.clj
+++ src/clj/hub/core.clj
@@ -6,17 +6,19 @@
    [hub.spotify :as spotify]
    [hub.discljord.core :as discord]
    [hub.server :as server]
-   [mount.core :as mount :refer [defstate]]
    [clojure.tools.logging :as log])
   (:gen-class))
 
+(def main-fn
+  {"conway"  conway/main
+   "dice"    dice/main  ;; in-file then out-file
+   "id3"     id3/main
+   "spotify" spotify/main
+   "server"  server/main
+   "discord" discord/main})
+
 ;; TODO: proper monorepo doesn't just switch on command
 (defn -main [command & args]
-  (case command
-    "conway"  (apply conway/main args)
-    "dice"    (apply dice/main args)  ;; in-file then out-file
-    "id3"     (apply id3/main args)
-    "spotify" (apply spotify/main args)
-    "server"  (apply server/main args)
-    "discord" (apply discord/main args)
-    (log/error "command must be one of [conway dice id3 spotify server]")))
+  (if-let [main (main-fn command)]
+    (apply main args)
+    (log/error "command must be one of " (keys main-fn))))
