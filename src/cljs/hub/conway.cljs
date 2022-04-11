6be315341687fc2ca519c1e2193dcb6717113a49
John Maruska <jackmaruska@gmail.com>
2022-04-11 17:19:41 -0500

    Implement autoplay for Conway on frontend

--- src/cljs/hub/conway.cljs
+++ src/cljs/hub/conway.cljs
@@ -6,7 +6,8 @@
 (def delay-ms 200)
 (def default-height 15)
 (def default-width 15)
-(def default-db {::game (game/random-seed default-height default-width)})
+(def default-db {::game     (game/random-seed default-height default-width)
+                 ::autoplay false})
 
 (def display-size "1em")
 
@@ -35,19 +36,53 @@
                ^{:key (str "x=" x)}
                [:td [cell [x y] val]]))]))]])
 
+(rf/reg-sub
+ ::game
+ (fn [db _]
+   (::game db)))
+
 (rf/reg-event-db
  ::step-game
  (fn [db _]
    (update db ::game game/play-round)))
 
 (rf/reg-sub
- ::game
+ ::autoplay
  (fn [db _]
-   (::game db)))
+   (::autoplay db)))
+
+;; step board and handle loop for autoplay, which is a side effect
+(rf/reg-event-fx
+ ::autoplay
+ (fn [{db :db} _]
+   (if (::autoplay db)
+     {:fx [[:dispatch [::step-game]]]
+      ::sleep-then-dispatch [#(rf/dispatch [::autoplay]) delay-ms]}
+     {:fx [[:dispatch [::step-game]]]})))
+
+;; this is that side effect
+(rf/reg-fx
+ ::sleep-then-run
+ (fn [[thunk delay]]
+   (js/setTimeout thunk delay)))
+
+;; user interaction to escape the forever loop
+(rf/reg-event-fx
+ ::toggle-autoplay
+ (fn [{db :db} [_ target]]
+   (let [autoplay (not (.-checked target))]
+     {:db (assoc db ::autoplay autoplay)
+      :fx [[:dispatch [::autoplay]]]})))
 
 (defn view []
-  (let [game (rf/subscribe [::game])]
+  (let [game      (rf/subscribe [::game])
+        autoplay? (rf/subscribe [::autoplay])]
     (fn []
       [:div {:class "conway"}
        [:button {:on-click #(rf/dispatch [::step-game])} "Step Game"]
+       [:div
+        "Autoplay"
+        [:input {:type      "checkbox"
+                 :checked   @autoplay?
+                 :on-change #(rf/dispatch [::toggle-autoplay (.-target %)])}]]
        [grid @game]])))
